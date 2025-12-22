613ed40a2bb927045662c2a0b9228fd170ec4684
DamyR <thomas@anvir.fr>
2025-12-22 23:52:18 +0100

    Update plugins.lua use witch-line

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -53,43 +53,11 @@ require("lazy").setup({
 			"Bekaboo/deadcolumn.nvim"
 	},
 	{
-	  "sontungexpt/sttusline",
+	  "sontungexpt/witch-line",
 	  branch = "main",
 	  dependencies = {
 	      "nvim-tree/nvim-web-devicons",
 	  },
-	  event = { "BufEnter" },
-	  config = function(_, opts)
-	      require("sttusline").setup {
-	          statusline_color = "StatusLine",
-	          -- | 1 | 2 | 3
-	          -- recommended: 3
-	          laststatus = 3,
-	          disabled = {
-	              filetypes = {
-	                  -- "NvimTree",
-	                  -- "lazy",
-	              },
-	              buftypes = {
-	                  -- "terminal",
-	              },
-	          },
-	          components = {
-	              "mode",
-	              "filename",
-	              "git-branch",
-	              "git-diff",
-	              "%=",
-	              "diagnostics",
-	              "lsps-formatters",
-	              "copilot",
-	              "indent",
-	              "encoding",
-	              "pos-cursor",
-	              "pos-cursor-progress",
-	          },
-	      }
-	  end,
 	},
 	{
 		"shellRaining/hlchunk.nvim",
